7fe0ee815a67f9f431d7229126ddfbdf8cf6997c
Elia Rice <eebrwriting@gmail.com>
2019-07-10 21:58:04 -0500

    Logic revisions and testing.

---
+++ src/main/java/com/revature/Menu.java
@@ -0,0 +1,25 @@
+package com.revature;
+
+class Menu {
+	public void title() {
+		System.out.println("Welcome to Meade Bank!");
+		System.out.println();
+	}
+	public void initial() {
+		System.out.println("Please enter a number so we might assist you.");
+		System.out.println("1: Log In");
+		System.out.println("2: Sign In");
+		System.out.println("3: Exit");
+	}
+	public void log() {
+		System.out.println("Please let us know who you are.");
+		System.out.println("1: I'm a client.");
+		System.out.println("2: I'm an employee of the bank.");
+		System.out.println("3: I'm an admin.");
+	}
+	public void sign() {
+		System.out.println("We're so happy you've decided to sign up with us!");
+		System.out.println("We just need a bit of information to get started.");
+		System.out.println("Please give us your name.");
+	}
+}
---
+++ src/test/java/com/revature/MainTest.java
@@ -0,0 +1,31 @@
+package com.revature;
+
+import java.util.Scanner;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		int option;
+		do {
+			Menu menu = new Menu();
+			menu.title();
+			menu.initial();
+			Scanner scanner = new Scanner(System.in);
+			option = scanner.nextInt();
+			switch (option) {
+			case 1: 
+				menu.title();
+				menu.log();
+				break;
+			case 2:
+				menu.title();
+				menu.sign();
+				break;
+			case 3:
+				System.out.println("Thank you for banking with us!");
+				scanner.close();
+				break;
+			}
+		} while(option != 3);
+	}
+}
